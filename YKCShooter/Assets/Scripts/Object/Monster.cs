af8304639df250251e7afa55e2a5ed18e3919f98
ch0i00135 <ch0i00135@gmail.com>
2024-10-28 23:11:47 +0900

    네비게이션 설정, 몬스터 적용

---
+++ YKCShooter/Assets/Scripts/Object/Monster.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Monster : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform player;
+    void Start()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
